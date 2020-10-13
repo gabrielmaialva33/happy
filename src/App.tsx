a0b6edc424df433a737a30c0d6a90f94aa3f0b9d
Gabriel Maia <gabrielmaialva@gmail.com>
2020-10-13 00:44:19 -0300

    feat: add landing css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import './styles/global.css';
+import './styles/pages/landing.css';
 
 const App: React.FC = () => (
-  <div>
+  <div id="page-landing">
     <h1>Happy</h1>
   </div>
 );
