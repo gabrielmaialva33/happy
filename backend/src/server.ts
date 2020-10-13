5e4dc095795c31ac4ffd528bd7d0da5f35844ad0
Gabriel Maia <gabrielmaialva@gmail.com>
2020-10-13 16:17:20 -0300

    chore: update eslint conf

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,4 +3,6 @@ import express from 'express';
 
 const app = express();
 
-app.listen(process.env.HTTP_PORT || 3333);
+app.listen(process.env.HTTP_PORT || 3333, () => {
+  console.log(` 🚀  Servidor iniciado. `);
+});
