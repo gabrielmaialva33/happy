893696352281cf63f3e95c5c225232bfa8663925
Gabriel Maia <gabrielmaialva@gmail.com>
2020-10-13 01:36:58 -0300

    refactor: add switch

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
-    <Route path="/" exact component={Landing} />
-    <Route path="/app" component={OrphanagesMap} />
+    <Switch>
+      <Route path="/" exact component={Landing} />
+      <Route path="/app" component={OrphanagesMap} />
+    </Switch>
   </BrowserRouter>
 );
 
