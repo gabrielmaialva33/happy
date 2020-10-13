ebe397460e1b3e5bcf4aec537d5301f8d4885a25
Gabriel Maia <gabrielmaialva@gmail.com>
2020-10-13 00:52:15 -0300

    feat: app elements on landing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,27 @@ import React from 'react';
 import './styles/global.css';
 import './styles/pages/landing.css';
 
+import logoimg from './images/logo.svg';
+
 const App: React.FC = () => (
   <div id="page-landing">
-    <h1>Happy</h1>
+    <div className="content-wrapper">
+      <img src={logoimg} alt="Happy" />
+
+      <main>
+        <h1>Leve felicidade para o mundo</h1>
+        <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+      </main>
+
+      <div className="locations">
+        <strong>Tatuí</strong>
+        <span>São Paulo</span>
+      </div>
+
+      <a href="" className="enter-app">
+        >
+      </a>
+    </div>
   </div>
 );
 
