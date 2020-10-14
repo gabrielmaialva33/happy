4835c48a2bf9f5eb2b3cc5f374c4256e9585da00
Gabriel Maia <gabrielmaialva@gmail.com>
2020-10-14 13:15:40 -0300

    feat: add downloaded pages

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import mapMakerImg from '../images/map-marker.svg';
 
 import 'leaflet/dist/leaflet.css';
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMakerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 const OrphanagesMap: React.FC = () => (
   <div id="page-map">
@@ -33,9 +42,23 @@ const OrphanagesMap: React.FC = () => (
       <TileLayer
         url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
       />
+
+      <Marker icon={mapIcon} position={[-24.0065961, -48.3803738]}>
+        <Popup
+          closeButton={false}
+          minWidth={240}
+          maxHeight={240}
+          className="map-popup"
+        >
+          Lar dos Garotos
+          <Link to="/orphanages/1">
+            <FiArrowRight size={20} color="#fff" />
+          </Link>
+        </Popup>
+      </Marker>
     </Map>
 
-    <Link to="/" className="create-orphanage">
+    <Link to="/orphanages/create" className="create-orphanage">
       <FiPlus size={32} color="#FFF" />
     </Link>
   </div>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,12 +3,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanages from './pages/Orphanage';
+import CreateOrphanagesMap from './pages/CreateOrphanage';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Landing} />
       <Route path="/app" component={OrphanagesMap} />
+
+      <Route path="/orphanages/create" component={CreateOrphanagesMap} />
+      <Route path="/orphanages/:id" component={Orphanages} />
     </Switch>
   </BrowserRouter>
 );
